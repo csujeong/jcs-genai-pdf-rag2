3cf001e20e6d8207b0ff9da8b157047f18548344
Jaspreet Singh <harshitbhattal@gmail.com>
2023-04-15 21:50:22 +0530

    added comments

--- model.py
+++ model.py
@@ -1,3 +1,7 @@
+# This code implements a summarization and questioning model using Streamlit, PyPDF2, OpenAI embeddings, and LangChain libraries.
+# It allows users to upload a PDF file, extract text from it, and then input additional text for analysis. Users can also input a query
+# to get answers from the analyzed text. The code utilizes functions for text summarization and question answering, and displays the
+# results using Streamlit interface.
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -9,18 +13,38 @@ import os
 
 # Function to summarize text
 def summarize_text(texts, docsearch, chain):
-    summry = docsearch.similarity_search(" ")
-    txt = chain.run(input_documents=summry, question="write summery in points within 150 words")
+    """
+    This function takes a list of texts, a document search object, and a question answering chain as input.
+    It performs text summarization by using the document search object to find the most similar document to an empty query,
+    and then passes the document to the question answering chain with a predefined question asking for a summary within 150 words.
+    The summarized text is returned as output.
+    """
+    summry = docsearch.similarity_search(" ")  # Find most similar document to empty query
+    txt = chain.run(input_documents=summry, question="write summery in points within 150 words")  # Run question answering chain
     return txt
+
 
 # Function to answer question
 def answer_question(query, docsearch, chain):
-    docs = docsearch.similarity_search(query)
-    txt = chain.run(input_documents=docs, question=query)
+    """
+    This function takes a query, a document search object, and a question answering chain as input.
+    It performs question answering by using the document search object to find the most similar documents to the input query,
+    and then passes the documents to the question answering chain with the input query as the question.
+    The answer to the question is returned as output.
+    """
+    docs = docsearch.similarity_search(query)  # Find most similar documents to input query
+    txt = chain.run(input_documents=docs, question=query)  # Run question answering chain
     return txt
 
 # Main function
 def main():
+    """
+    This is the main function that runs the Streamlit application.
+    It displays a title, allows users to input their API key, upload a PDF file, and input additional text for analysis.
+    It also allows users to input a query for question answering.
+    The function calls the summarize_text() and answer_question() functions to perform text summarization and question answering
+    respectively, and displays the results using Streamlit interface.
+    """
     st.title('Summarization and Questioning Model')
 
     api_key = st.text_input('Your Key', placeholder="Enter Your key")
@@ -55,15 +79,23 @@ def main():
 
     col1, col2, col3 = st.columns(3)
 
+    # Button click event and input validation
     if col2.button('Submit') and (uploaded_file is not None or temp_data!="data : ") :
         embeddings = OpenAIEmbeddings()
         docsearch = FAISS.from_texts(texts, embeddings)
         chain = load_qa_chain(OpenAI(), chain_type="stuff")
 
-        if 'summry' not in st.session_state or  query == "query : ":  # Check if summary is already computed
+        # Streamlit session state usage
+        if 'summry' not in st.session_state or  query == "query : ":
+            """
+            This condition checks if the 'summry' key is not present in the Streamlit session state, or if the input query is empty.
+            If either of these conditions is true, it means that the summary is not computed yet or the user has cleared the query input.
+            In such cases, the summarize_text() function is called to compute the summary and store it in the 'summry' key of the session state.
+            """
             # Call summarize_text() function
             st.session_state['summry'] = summarize_text(texts, docsearch, chain)
             st.write('Summary:', st.session_state['summry'])
+
 
         # Call answer_question() function
         if query != "query : ":
